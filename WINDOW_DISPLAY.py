e71624c07d74458b396e29324458a350c48aa1d4
ljbpro <30717429+ljbpro@users.noreply.github.com>
2018-07-01 02:07:52 +0900

    Delete WINDOW_DISPLAY.py

--- WINDOW_DISPLAY.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-window = tkinter.Tk() 
-label = tkinter.Label(window, text = "サンプル") 
-label.pack()    # --- (1) 
-button = tkinter.Button(window, text = "ボタンです。押しても何も起きません") 
-button.pack() 
- 
-window.mainloop() 
